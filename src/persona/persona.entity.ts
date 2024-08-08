684bcd394dc2667c15f818a0b80c95ecb1b8902c
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2024-08-08 12:36:33 -0300

    Add login API & bcrypt dependency

--- src/persona/persona.entity.ts
+++ src/persona/persona.entity.ts
@@ -1,26 +1,31 @@
-import { Cascade, Collection, Entity, OneToMany, Property } from '@mikro-orm/core';
-import crypto from 'node:crypto';
+import {
+  Cascade,
+  Collection,
+  Entity,
+  OneToMany,
+  Property,
+} from '@mikro-orm/core';
 import { BaseEntity } from '../shared/base-entity.entity.js';
 import { Producto } from '../producto/producto.entity.js';
 
-
 @Entity()
-export class Persona extends BaseEntity{
+export class Persona extends BaseEntity {
+  @Property({ nullable: false, unique: false })
+  nombre!: string;
+  @Property({ nullable: false, unique: false })
+  apellido!: string;
 
-    @Property({nullable:false,unique:false})
-    nombre !: string
-    @Property({nullable:false,unique:false})
-    apellido!: string
-    
-    @Property({nullable:true,unique:false})
-    telefono?: string
-    
-    @Property({nullable:true,unique:false})
-    mail!: string
-    
-    @OneToMany(() => Producto,
-    (producto) => producto.persona,
-    {cascade: [Cascade.ALL]})
-    
-    prods_publicados = new Collection<Producto>(this)
+  @Property({ nullable: true, unique: false })
+  telefono?: string;
+
+  @Property({ nullable: true, unique: false })
+  mail!: string;
+
+  @OneToMany(() => Producto, (producto) => producto.persona, {
+    cascade: [Cascade.ALL],
+  })
+  prods_publicados = new Collection<Producto>(this);
+
+  @Property({ nullable: false, hidden: true })
+  password!: string;
 }
