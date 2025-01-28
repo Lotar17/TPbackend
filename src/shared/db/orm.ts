9d1f908a73292ba69941f4373026dc13ecf89cf1
Ignacio Di Martino <nachojdimartino@gmail.com>
2025-01-28 19:34:13 -0300

    agrego cambios de rama main

--- src/shared/db/orm.ts
+++ src/shared/db/orm.ts
@@ -15,7 +15,7 @@ export const orm = await MikroORM.init({
   entitiesTs: ['src/**/*.entity.ts'],
   dbName: 'tpbackend',
   type: 'mongo',
-  clientUrl: `${DB}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`,
+  clientUrl: `mongodb://127.0.0.1:27017`,
   highlighter: new MongoHighlighter(),
   debug: true,
   schemaGenerator: {
