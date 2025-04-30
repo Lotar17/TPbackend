3a630480acc96746f33acdf9d218b868828bf104
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2025-04-30 10:19:25 -0300

    fix: bugs de merge

--- src/persona/persona.controller.ts
+++ src/persona/persona.controller.ts
@@ -5,7 +5,7 @@ import { error } from 'console';
 import { Populate } from '@mikro-orm/core';
 import bcrypt from 'bcrypt';
 import nodemailer from 'nodemailer';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import { Localidad } from '../localidad/localidad.entity.js';
 import { Direccion } from '../direccion/direccion.entity.js';
 import { ValidationError } from '../Errores/validationErrors.js';
@@ -13,11 +13,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
-
-
 const em = orm.em;
-export const SECRET_JWT_KEY = process.env.SECRET_JWT_KEY || 'nachovalenlotar'
+export const SECRET_JWT_KEY = process.env.SECRET_JWT_KEY || 'nachovalenlotar';
 
 function sanitizePersonaInput(req: Request, res: Response, next: NextFunction) {
   req.body.sanitizedInput = {
@@ -26,9 +23,11 @@ function sanitizePersonaInput(req: Request, res: Response, next: NextFunction) {
     telefono: req.body.telefono,
     mail: req.body.mail,
     prods_publicados: req.body.prods_publicados,
-    password: req.body.password ? bcrypt.hashSync(req.body.password, 10) : undefined,// Si en el put o en el patch no se pone un password tira error
-    passwordAnterior:req.body.passwordAnterior,
-    passwordNueva:req.body.passwordNueva,
+    password: req.body.password
+      ? bcrypt.hashSync(req.body.password, 10)
+      : undefined, // Si en el put o en el patch no se pone un password tira error
+    passwordAnterior: req.body.passwordAnterior,
+    passwordNueva: req.body.passwordNueva,
     rol: req.body.rol,
     carrito: req.body.carrito,
     direccion: req.body.direccion,
@@ -71,8 +70,17 @@ async function getOne(req: Request, res: Response) {
       Persona,
       { id },
 
-      { populate: ['prods_publicados','estados_empleados.seguimiento.cliente.direccion.localidad','estados_empleados.seguimiento.item.producto.persona.direccion.localidad','estados_empleados.localidad','direccion.localidad','compras.direccion','estados_empleados.seguimiento.item.compra.direccion.localidad'] }
-
+      {
+        populate: [
+          'prods_publicados',
+          'estados_empleados.seguimiento.cliente.direccion.localidad',
+          'estados_empleados.seguimiento.item.producto.persona.direccion.localidad',
+          'estados_empleados.localidad',
+          'direccion.localidad',
+          'compras.direccion',
+          'estados_empleados.seguimiento.item.compra.direccion.localidad',
+        ],
+      }
     );
     // const { password, ...personaData } = persona;
     return res.status(200).json({ message: 'found person', data: persona });
@@ -85,9 +93,11 @@ async function getPersonaByEmail(req: Request, res: Response) {
   try {
     const email = decodeURIComponent(req.params.email);
     console.log('Email recibido en backend:', email);
-    
+
     const persona = await em.findOneOrFail(Persona, { mail: email });
-    const token = jwt.sign({id: persona.id}, SECRET_JWT_KEY, {expiresIn: '5m'});
+    const token = jwt.sign({ id: persona.id }, SECRET_JWT_KEY, {
+      expiresIn: '5m',
+    });
     const link = `http://localhost:4200/restablecer-contrasena?token=${token}`;
 
     const transporter = nodemailer.createTransport({
@@ -97,22 +107,24 @@ async function getPersonaByEmail(req: Request, res: Response) {
         pass: process.env.GMAIL_PASS,
       },
     });
-    
+
     await transporter.sendMail({
       from: process.env.GMAIL_USER,
       to: email,
       subject: 'Restablecer contraseña',
       text: 'Restablecer contraseña',
       html: `<p>Hola ${persona.nombre},</p>
-      <a href="${link}">Hacé clic aqui para restablecer tu contraseña</a><p>El enlace vence en 5 minutos:</p>`,});
-    
+      <a href="${link}">Hacé clic aqui para restablecer tu contraseña</a><p>El enlace vence en 5 minutos:</p>`,
+    });
+
     return res.status(200).json({ message: 'Correo enviado', data: persona });
   } catch (error: any) {
     console.error(error);
-    return res.status(500).json({ message: 'Error inesperado', error: error.message });
+    return res
+      .status(500)
+      .json({ message: 'Error inesperado', error: error.message });
   }
 }
-
 
 async function add(req: Request, res: Response) {
   try {
@@ -166,35 +178,38 @@ async function add(req: Request, res: Response) {
 
 async function update(req: Request, res: Response) {
   try {
-    console.log('Direccion',req.body.sanitizedInput.direccion)
+    console.log('Direccion', req.body.sanitizedInput.direccion);
     const id = req.params.id;
     const personaToUpdate = await em.findOneOrFail(Persona, { id });
 
     const mail = req.body.sanitizedInput.mail;
-  
+
     if (mail !== undefined) {
       const mailRepetido = await em.findOne(Persona, { mail: mail });
       if (mailRepetido && mailRepetido.id !== personaToUpdate.id) {
-        throw new ValidationError('No se puede actualizar con un mail ya existente');
+        throw new ValidationError(
+          'No se puede actualizar con un mail ya existente'
+        );
       }
     }
-    
+
     em.assign(personaToUpdate, req.body.sanitizedInput);
     await em.flush();
     return res
       .status(200)
       .json({ message: 'Person updated succesfully !', data: personaToUpdate });
   } catch (error: any) {
-
     if (error instanceof ValidationError) {
       // Este es el mensaje específico que se pasará al frontend
       res.status(400).send({ message: error.message, result: false });
     } else {
       // Otros tipos de errores generales
-      res.status(500).json({ message: 'Ocurrió un error en el servidor', result: false });
+      res
+        .status(500)
+        .json({ message: 'Ocurrió un error en el servidor', result: false });
     }
-
-  }}
+  }
+}
 
 async function remove(req: Request, res: Response) {
   try {
@@ -224,10 +239,9 @@ async function updatePassword(req: Request, res: Response) {
     }
 
     user.password = await bcrypt.hash(passwordNueva, 10);
-    console.log("Nuevo hash:", user.password);
-em.persist(user); // Forzamos que MikroORM lo tome como una entidad a guardar
-await em.flush();
-
+    console.log('Nuevo hash:', user.password);
+    em.persist(user); // Forzamos que MikroORM lo tome como una entidad a guardar
+    await em.flush();
 
     return res.status(200).json({
       message: 'Password updated successfully!',
@@ -238,7 +252,9 @@ await em.flush();
       res.status(400).send({ message: error.message, result: false });
     } else {
       console.error(error);
-      res.status(500).send({ message: 'Error interno del servidor', result: false });
+      res
+        .status(500)
+        .send({ message: 'Error interno del servidor', result: false });
     }
   }
 }
@@ -250,7 +266,6 @@ export {
   update,
   sanitizePersonaInput as sanitizeCharacterInput,
   remove,
-  getPersonaByEmail
-  updatePassword
-
+  getPersonaByEmail,
+  updatePassword,
 };
